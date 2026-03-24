a2dd6c9dc30d41cada4f911a5c3b7bf50990b3b4
Francesco-Baroni <francesco.baroni-1@unitn.it>
2026-03-24 12:28:36 +0100

    remove unused plot functions and arithmetic imports

--- nnodely/__init__.py
+++ nnodely/__init__.py
@@ -10,13 +10,12 @@ from nnodely.stream import Stream
 from nnodely.layer import Layer, LayerBase
 from nnodely.fir import Fir
 from nnodely.loop import Loop
-from nnodely.arithmetic import Add, Subtract, Multiply, Divide
+#from nnodely.arithmetic import Add, Subtract, Multiply, Divide
 from nnodely.parameter import Parameter
 from nnodely.output import Output
 from nnodely.model import Model
 
 __all__ = [
     'Layer', 'LayerBase', 'Input', 'Stream', 'Fir', 'Parameter', 'Loop',
-    'Add', 'Subtract', 'Multiply', 'Divide',
     'Output', 'Model'
 ]
