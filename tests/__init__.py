ff4c0a738714e6f757f06fa9f7352b5149e21ae1
MrMandarino <giovanni_scialla1999@outlook.it>
2026-04-16 15:02:32 +0200

    chore: removed old tests

--- tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-# Imposta un backend non-GUI solo su Windows
-if sys.platform.startswith("win"):
-    import matplotlib
-
-    matplotlib.use("Agg")
---
+++ tests/test_basic.py
@@ -0,0 +1,2 @@
+def test_version():
+    assert 3 == 3
